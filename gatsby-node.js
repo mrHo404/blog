c5be2d864948d3af13d9aa4bf169c5da4b5da9c5
Chi-Tin Ho <chi-tin.ho@valtech.com>
2020-03-03 16:27:57 +0100

    added tag posts page

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,7 +18,8 @@ exports.createPages = ({ graphql, actions, reporter }) => {
   const { createPage } = actions
   const templates = {
     singlePost: path.resolve("src/templates/single-post.js"),
-    tagsPage: path.resolve("src/templates/tags-page.js")
+    tagsPage: path.resolve("src/templates/tags-page.js"),
+    tagPosts: path.resolve("src/templates/tag-posts.js")
   }
 
   return graphql(`
@@ -82,6 +83,17 @@ exports.createPages = ({ graphql, actions, reporter }) => {
         tags,
         tagPostCounts
       }
+    })
+
+    //create Tag Post Pages
+    tags.forEach(tag =>{
+      createPage({
+        path:`/tag/${slugify(tag)}`,
+        component:templates.tagPosts,
+        context: {
+          tag
+        }
+      })
     })
   })
 }
