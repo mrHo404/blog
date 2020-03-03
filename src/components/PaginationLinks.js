1abcea5bcafa5dc13f66b81ae1eaf26d8b9db228
Chi-Tin Ho <chi-tin.ho@valtech.com>
2020-03-03 17:37:01 +0100

    removed unnecessary hrefs

--- src/components/PaginationLinks.js
+++ src/components/PaginationLinks.js
@@ -11,7 +11,7 @@ const PaginationLinks = ({ currentPage, nrOfPages }) => {
     <Pagination aria-label="Page navigation example">
       {isFirst ? (
         <PaginationItem disabled>
-          <PaginationLink previous href="/" />
+          <PaginationLink previous/>
         </PaginationItem>
       ) : (
         <PaginationItem>
@@ -35,7 +35,7 @@ const PaginationLinks = ({ currentPage, nrOfPages }) => {
       )}
       {isLast ? (
         <PaginationItem disabled>
-          <PaginationLink next href="/" />
+          <PaginationLink next/>
         </PaginationItem>
       ) : (
         <PaginationItem>
