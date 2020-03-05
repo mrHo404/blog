3b74f2b365a0965a4ba9e0aa74b4facdb692b456
Chi-Tin Ho <chi-tin.ho@valtech.com>
2020-03-05 14:17:48 +0100

    reworked minor styling issues restyled 404 page added query for 404 image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,41 @@
 import React from "react"
-import Oops from './../assets/oops.png';
-import {FaRegSadTear} from 'react-icons/fa';
-
+import Img from "gatsby-image"
 import Layout from "../components/layout"
-import { Link } from "gatsby"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap"
 import SEO from "../components/seo"
 
-export default () => (
-  <Layout pageTitle='404 - Not Found'>
-    <SEO title="404 - Not found" />
-    <underConstruction>
-      <div className='construction-text'>
-        <p>Sorry diese Seite muss erst noch gebaut werden </p>
-        <FaRegSadTear/>
-        <p>Wir löten so schnell wir können!</p>
-        <Link className=' btn btn-primary text-uppercase' to={'/'}>Go Home</Link>
-      </div>
-      <img src={Oops} alt={''}/>
-    </underConstruction>
-  </Layout>
-)
+export default () => {
+  const notFoundQuery = useStaticQuery(graphql`
+    query notFoundQuery {
+      file(relativePath: { eq: "404.png" }) {
+        id
+        childImageSharp {
+          fluid(maxWidth: 1920) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout pageTitle="404 - Oh Nein!">
+      <SEO title="404 - Not found" />
+      <Card>
+        <CardBody>
+          <CardTitle> Da ist wohl was schief gelaufen!</CardTitle>
+          <CardSubtitle>
+            Sorry diese Seite muss erst noch gebaut werden.
+            <br />
+            Wir löten so schnell wir können!
+          </CardSubtitle>
+          <Img fluid={notFoundQuery.file.childImageSharp.fluid} />
+          <Link className=" btn btn-primary text-uppercase" to={"/"}>
+            Zurück zur Hauptseite
+          </Link>
+        </CardBody>
+      </Card>
+    </Layout>
+  )
+}
